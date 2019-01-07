e14317dcc9db3b9c143635626f00c4a66037f46e
Nikola Maljenovic <32510744+NikolaMC@users.noreply.github.com>
2019-01-07 11:14:19 +0100

    Add project files.

---
+++ WU18_Inlamning_NikolaMaljenovic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WU18_Inlamning_NikolaMaljenovic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
